2d30f0372fcc4200b48e2e0c9839aaccaa73da79
cortland.finch <cortlandfinch@Cortlands-MacBook-Pro.local>
2022-10-19 19:25:37 -0400

    update footer and package.json

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,25 +8,25 @@ const Footer = () => {
         <footer className='fixed bottom-0 left-0 h-[60px] z-20 p-4 w-full bg-[#2b2b2b] border-t border-[#6c757d] text-[#e9ecef] shadow md:flex md:items-center md:justify-between md:p-6'>
             <ul className='hidden md:flex nav-bar-menu mx-auto space-x-10'>
                 <li className='footer-link'>
-                    <a className='flex justify-between items-center w-full text-[#e9ecef]' href='/'>Cortland Finch</a>
+                    <a className='flex justify-between items-center w-full' href='/'>Cortland Finch</a>
                 </li>
                 <li className='footer-link flex justify-between items-center'>
-                    <a className='flex justify-between items-center w-full text-[#e9ecef]' href='https://www.linkedin.com/in/cortland-finch/'>
+                    <a className='flex justify-between items-center w-full' href='https://www.linkedin.com/in/cortland-finch/' target='_blank'>
                         LinkedIn <FaLinkedin size={30} />
                     </a>
                 </li>
                 <li className='footer-link'>
-                    <a className='flex justify-between items-center w-full text-[#e9ecef]' href='https://github.com/cortlandfinch'>
+                    <a className='flex justify-between items-center w-full' href='https://github.com/cortlandfinch' target='_blank'>
                         GitHub <FaGithub size={30} />
                     </a>
                 </li>
                 <li className='footer-link'>
-                    <a className='flex justify-between items-center w-full text-[#e9ecef]' href='/contact'>
+                    <a className='flex justify-between items-center w-full' href='/contact'>
                         Email <HiOutlineMail size={30} />
                     </a>
                 </li>
                 <li className='footer-link'>
-                    <a className='flex justify-between items-center w-full text-[#e9ecef]' href='/resume'>
+                    <a className='flex justify-between items-center w-full' href='/resume'>
                         Resume <BsFillPersonLinesFill size={30} />
                     </a>
                 </li>
